bec4498feb6e16fd3dd60b6252843b613e0645c2
yupolischuk <yumuscletank@gmail.com>
2018-08-23 12:04:05 +0300

    Added double-ended queue

--- BasicDataStructures/circular_queue.py
+++ BasicDataStructures/circular_queue.py
@@ -6,6 +6,10 @@ class Node:
 class MyCircularQueue:
 
     def __init__(self, k):
+        """
+        Initialize your data structure here. Set the size of the queue to be k.
+        :type k: int
+        """
         self.size = k
         self.curSize = 0
         self.head = self.tail = Node(-1)
@@ -13,6 +17,11 @@ class MyCircularQueue:
         self.tail.pre = self.head
 
     def enQueue(self, value):
+        """
+        Insert an element into the circular queue. Return true if the operation is successful.
+        :type value: int
+        :rtype: bool
+        """
         if self.curSize < self.size:
             node = Node(value)
             node.pre = self.tail.pre
@@ -23,6 +32,10 @@ class MyCircularQueue:
         return False
 
     def deQueue(self):
+        """
+        Delete an element from the circular queue. Return true if the operation is successful.
+        :rtype: bool
+        """
         if self.curSize > 0:
             node = self.head.next
             node.pre.next = node.next
@@ -32,23 +45,39 @@ class MyCircularQueue:
         return False
 
     def Front(self):
+        """
+        Get the front item from the queue.
+        :rtype: int
+        """
         return self.head.next.val
 
     def Rear(self):
+        """
+        Get the last item from the queue.
+        :rtype: int
+        """
         return self.tail.pre.val
 
     def isEmpty(self):
+        """
+        Checks whether the circular queue is empty or not.
+        :rtype: bool
+        """
         return self.curSize == 0
 
     def isFull(self):
+        """
+        Checks whether the circular queue is full or not.
+        :rtype: bool
+        """
         return self.curSize == self.size
 
 # Your MyCircularQueue object will be instantiated and called as such:
-# obj = MyCircularQueue(k)
-# param_1 = obj.enQueue(value)
-# param_2 = obj.deQueue()
-# param_3 = obj.Front()
-# param_4 = obj.Rear()
-# param_5 = obj.isEmpty()
-# param_6 = obj.isFull()
-
+k = 3
+obj = MyCircularQueue(k)
+param_1 = obj.enQueue(value)
+param_2 = obj.deQueue()
+param_3 = obj.Front()
+param_4 = obj.Rear()
+param_5 = obj.isEmpty()
+param_6 = obj.isFull()
