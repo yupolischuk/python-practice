9df3f58dad1e994341aa4097522e25242ad88ecf
yupolischuk <yumuscletank@gmail.com>
2018-10-24 10:44:36 +0300

    Added cmd for staowatch

--- oo_design/stopwatch.py
+++ oo_design/stopwatch.py
@@ -1,5 +1,5 @@
 import datetime
-
+from cmd import Cmd
 
 class Stopwatch:
     def __init__(self):
@@ -14,3 +14,31 @@ class Stopwatch:
 
     def get_lapsed_time(self):
         return self.stop_time - self.start_time
+
+
+
+class Prompt(Cmd):
+    def __init__(self):
+        Cmd.__init__(self)
+        self.sw = Stopwatch()
+
+    def do_start(self, args):
+        print("started")
+        self.sw.start()
+
+    def do_stop(self, args):
+        print("stopped")
+        self.sw.stop()
+        print(self.sw.get_lapsed_time())
+
+    def do_quit(self, args):
+        """Quits the program."""
+        print("Quitting.")
+        raise SystemExit
+
+
+if __name__ == '__main__':
+    prompt = Prompt()
+    prompt.prompt = '> '
+    prompt.cmdloop('Starting prompt...')
+
